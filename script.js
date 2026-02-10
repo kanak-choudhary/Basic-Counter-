f1f5d5ccd3aa9f599248bbe1f6fa47be43da42ad
Kanak Choudhary <kanak25.choudhary@gmail.com>
2026-02-10 21:40:41 +0530

    first commit

--- script.js
+++ script.js
@@ -13,15 +13,29 @@ let count = 0;
 increaseBtn[0].addEventListener("click", function () {
   count++;
   counterDisplay.textContent = count;
+   localStorage.setItem("counterValue", count);
 });
 
 // decrease button
 decreaseBtn[0].addEventListener("click", function () {
   count--;
   counterDisplay.textContent = count;
+   localStorage.setItem("counterValue", count);
 });
 
 resetBtn[0].addEventListener("click", function () {
   count = 0;
   counterDisplay.textContent = count;
+   localStorage.setItem("counterValue", count);
 });
+
+window.onload= function (){
+
+  let savedCount = localStorage.getItem("counterValue");
+
+  if (savedCount !== null) {
+    count = Number(savedCount);
+  
+  counterDisplay.textContent = count;
+}
+}
